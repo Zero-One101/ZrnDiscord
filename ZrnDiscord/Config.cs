6c826b010dd627d5ee917d7cf8eea6166991c7f4
Stephen Badrick <stephenbadrick@gmail.com>
2018-08-25 17:25:01 +0100

    Connecting and ping functionality

---
+++ ZrnDiscord/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZrnDiscord
+{
+    static class Config
+    {
+        public static string AuthToken { get; private set; }
+        public static char ControlChar { get; private set; }
+
+        public static void ReadConfig()
+        {
+            var config = File.ReadAllLines("config.cfg");
+            Dictionary<String, String> props = new Dictionary<string, string>();
+
+            for (var i = 0; i < config.Length; i++)
+            {
+                var prop = config[i].Split('=');
+                props.Add(prop[0], prop[1]);
+            }
+
+            AuthToken = props["AuthToken"];
+            ControlChar = props["ControlChar"].ToCharArray()[0];
+        }
+    }
+}
---
+++ ZrnDiscord/Program.cs
@@ -0,0 +1,63 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace ZrnDiscord
+{
+    class Program
+    {
+        private DiscordSocketClient client;
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            client.Log += Log;
+            client.MessageReceived += ReceiveMessage;
+            Config.ReadConfig();
+
+            var token = Config.AuthToken;
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString(padSource: msg.Source.Length));
+            return Task.CompletedTask;
+        }
+
+        private void LogInfo(String source, String msg)
+        {
+            var lMsg = new LogMessage(LogSeverity.Info, source, msg);
+            Log(lMsg);
+        }
+
+        private void LogMessage(SocketMessage message)
+        {
+            var source = string.Format("#{0} <{1}>:", message.Channel, message.Author);
+            LogInfo(source, message.Content);
+        }
+
+        private async Task ReceiveMessage(SocketMessage message)
+        {
+            LogMessage(message);
+            if (message.Content.StartsWith(Config.ControlChar.ToString()) && message.Author.Id != client.CurrentUser.Id)
+            {
+                await HandleCommand(message);
+            }
+        }
+
+        private async Task HandleCommand(SocketMessage message)
+        {
+            if (message.Content == Config.ControlChar + "ping")
+            {
+                await message.Channel.SendMessageAsync("%ping");
+            }
+        }
+    }
+}
